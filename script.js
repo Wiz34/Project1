e01da41c5c40fc2396ba777764aa0efe03839112
Dwayne Thomas <WizHome@Dwaynes-MacBook-Air.local>
2019-06-12 19:38:30 -0400

    additional details

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const Base_URL = `https://api.waqi.info`
 //const index = document.querySelector("#index_Data");
 //const btn = document.querySelector("button")
 
+let city;
 
-
-const render = (city) => {
+const render = () => {
 
   const citylist = document.querySelector('.city-details')
   citylist.innerHTML = '';
@@ -16,52 +16,40 @@ const render = (city) => {
   const time = city.time.s
   const long_lat = city.city.geo
   const station_name = city.attributions[0].name
-  console.log(city)
 
+  console.log(city)
   const cityDiv = document.createElement('div')
   cityDiv.innerHTML = `
-      <h1>${cityName}</h1>
-      <h3>${AQI}</h3>
-      <button id='inbutt'> more details</button >
-      `
+  <h1>${cityName}</h1>
+  <h3>${AQI}</h3>
+  <h4>${time}</h4>
+  <h4>${long_lat}</h4>
+  <h4>${station_name}</h4>
+  <button id='inbutt'> more details</button >
+  `;
   const detailsBtn = cityDiv.querySelector('button')
 
-  detailsBtn.addEventListener('click', async () => {
-
-
-
+  detailsBtn.addEventListener('click', () => {
     const details = document.querySelector('.additional-details')
     details.innerHTML = ''
 
+    const objArr = Object.keys(city.iaqi);
 
-
-    const detailContainer = document.createElement('div')
-    detailContainer.innerHTML = `
-      <h4>${time}</h4>
-      <h4>${long_lat}
-      <h4>${station_name}
-    
-      
-
-
-      `
-
-    details.appendChild(detailContainer)
-
-
-
+    objArr.forEach((key) => {
+      const detailContainer = document.createElement('div')
+      detailContainer.innerHTML = `
+        <h4>${key}: ${city.iaqi[key].v}</h4>
+        `
+      details.appendChild(detailContainer)
+    })
   })
   citylist.appendChild(cityDiv)
 }
 
-
-
-
-
 const searchFetch = async (input) => {
   const response = await axios.get(`${Base_URL}/feed/${input}/?token=${API_KEY}`)
-  const data = response.data.data
-  render(data)
+  city = response.data.data
+  render();
 };
 
 const handleInput = () => {
@@ -71,5 +59,3 @@ const handleInput = () => {
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', handleInput);
-
-
